3b5166b8b0fa2c81dcf027e183b8a83c0aac1914
James Okonkwo <68653373+Onyetech@users.noreply.github.com>
2022-03-19 16:56:34 +0100

    modified Error code class

--- src/main/java/com/ikenna/statisticsrestfulapi/controller/TransactionController.java
+++ src/main/java/com/ikenna/statisticsrestfulapi/controller/TransactionController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/transactions")
@@ -18,7 +16,7 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
-    @PostMapping
+    @PostMapping("/post")
     public ResponseEntity<Void> addTransaction(@RequestBody Transaction transaction) {
         try {
             transactionService.addTransaction(transaction);
@@ -31,5 +29,11 @@ public class TransactionController {
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
 
     }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> deleteUser(){
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/com/ikenna/statisticsrestfulapi/model/ErrorCodes.java
+++ src/main/java/com/ikenna/statisticsrestfulapi/model/ErrorCodes.java
@@ -1,14 +1,16 @@
 package com.ikenna.statisticsrestfulapi.model;
 
 public enum ErrorCodes {
+    // something here
 
-    MISSING_PARAMETER_AMOUNT(1001, "Parameter missing - amount"),
-    MISSING_PARAMETER_TIMESTAMP(1002, "Parameter missing - timestamp"),
-    INVALID_PARAMETER_AMOUNT(1003, "Parameter invalid - amount"),
-    INVALID_PARAMETER_TIMESTAMP(1004, "Parameter invalid - amount"),
-    CLIENT_SERVER_OUT_OF_SYNC(1005, "Client Server not in sync"),
-    STALE_TIMESTAMP(1006, "Clients are too slow"),
-    INTERNAL_SERVER_ERROR(9999, "Internal Server Error");
+
+    MISSING_PARAMETER_AMOUNT(422, "Parameter missing - amount"),
+    MISSING_PARAMETER_TIMESTAMP(422, "Parameter missing - timestamp"),
+    INVALID_PARAMETER_AMOUNT(400, "Parameter invalid - amount"),
+    INVALID_PARAMETER_TIMESTAMP(400, "Parameter invalid - amount"),
+    CLIENT_SERVER_OUT_OF_SYNC(601, "Client Server not in sync"),
+    STALE_TIMESTAMP(204, "Clients are too slow"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private Integer errorCode;
     private String errorMessage;
