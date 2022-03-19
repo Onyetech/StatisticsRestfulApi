8441992f5923f98063320e87a7cdcd0d5d2c4f2f
James Okonkwo <68653373+Onyetech@users.noreply.github.com>
2022-03-19 16:07:39 +0100

    testing

--- src/main/java/com/ikenna/statisticsrestfulapi/controller/StatisticsController.java
+++ src/main/java/com/ikenna/statisticsrestfulapi/controller/StatisticsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/statistics")
 public class StatisticsController {
-
+// testing
     @Autowired
     private StatisticsService statisticsService;
 
